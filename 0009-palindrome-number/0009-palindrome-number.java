a8b3a0de64cfefafeab4b05b6420310fe6302fd1
Greeshma Sathvika Meesala <meesalasathvika2005@gmail.com>
2025-09-20 21:13:55 +0530

    Time: 4 ms (100%), Space: 44 MB (76.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int ans=x;
         if(x<0){
             return false;
         }
+        int dup=x;
         int rev=0;
-        while(x>0){
+        while(x!=0){
             int digit=x%10;
+            rev=rev*10+digit;
             x/=10;
-            rev=(rev*10)+digit;
         }
-        if(rev==ans){
+        if(dup==rev){
             return true;
         }
-        else{
-            return false;
-
-        }
+        return false;
     }
 }
